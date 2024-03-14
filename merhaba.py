f01816b29375ed146ba88bcf388dfeaf2c5267fb
Emirhan <160943241+EmirhanSerttas@users.noreply.github.com>
2024-03-14 22:33:32 +0300

    Update merhaba.py

--- merhaba.py
+++ merhaba.py
@@ -1,1 +1,28 @@
-print(merhaba)
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 1):
+    await ctx.send("ne hehliyon ##### " * count_heh)
+
+@bot.command()
+async def topla(ctx,sayı=0,sayı2=0):
+    x=sayı+sayı2
+    await ctx.send(f"verdiğiniz sayıların toplamı {x} tir")
+
+bot.run("IMPORTTOKEN")
